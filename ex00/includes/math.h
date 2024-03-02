0ab8adb87289e0795e9ece3ee2ccc96434546c0b
nsainto <nsainton@student.42.fr>
2024-03-02 14:49:21 +0100

    feat(templates): :construction: Implemented functions and added test main :construction:

---
+++ ex00/includes/math.h
@@ -0,0 +1,12 @@
+#ifndef __MATH_H__
+# define __MATH_H__
+
+template <typename T>
+void	swap(T & a, T & b);
+
+template <typename T>
+T		min(T & a, T & b);
+
+template <typename T>
+T		max(T & a, T & b);
+#endif
