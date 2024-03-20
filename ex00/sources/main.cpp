455bcf51ca665186410df9e215dca09920b36c57
lazydev <nsainton@student.42.fr>
2024-03-20 21:13:09 +0100

    feat(main, ex00): :sparkles: Added tests for const type qualifier in min and max templates :sparkles:

--- ex00/sources/main.cpp
+++ ex00/sources/main.cpp
@@ -4,19 +4,21 @@
 int main( void ) {
 	int a = 2;
 	int b = 3;
+	const int e = 2;
 
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min( e, b ) = " << ::min( e, b ) << std::endl;
+	std::cout << "max( e, b ) = " << ::max( e, b ) << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	const std::string	f="chaine1";
 
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min( f, d ) = " << ::min( f, d ) << std::endl;
+	std::cout << "max( f, d ) = " << ::max( f, d ) << std::endl;
 
 	return 0;
 }
